b510138cfe8bbef49a08917448db46f3f7ba65c2
kyosukemine <73222173+kyosukemine@users.noreply.github.com>
2021-11-02 16:16:33 +0900

    Update Access_camera_WITHOUT_OpenCV.py

--- pyfile/Access_camera_WITHOUT_OpenCV.py
+++ pyfile/Access_camera_WITHOUT_OpenCV.py
@@ -5,22 +5,16 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 
-
-
-
-
 pygame.init()
 pygame.camera.init()
 camlist = pygame.camera.list_cameras()
 if camlist:
     cam = pygame.camera.Camera(camlist[0],(640,480))
 
+    
 cam.start()
 
-
-
 size = width, height = 640, 480
-
 screen = pygame.display.set_mode(size)
 
 BLACK = (0, 0, 0)
@@ -28,7 +22,6 @@ ORIGIN = (0, 0)
 while 1:
   for event in pygame.event.get():
     if event.type == pygame.QUIT: sys.exit()
-
   image = cam.get_image()
   screen.fill(BLACK)
   screen.blit(image, ORIGIN)

fdc293f63f78e23e85225c4229817284280e85e7
kyosukemine <73222173+kyosukemine@users.noreply.github.com>
2021-11-04 19:00:52 +0900

    Update utils.py

--- detect_object/utils.py
+++ detect_object/utils.py
@@ -65,13 +65,13 @@ def visualize(
     #             _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
     
     
-    font = ImageFont.load_default()
+    font = ImageFont.load_default(size=_FONT_SIZE)
     text = class_name + ' (' + str(probability) + ')'
-    top, left = start_point
+    left, top = start_point
     txpos = (left, top-_FONT_SIZE-_FONT_THICKNESS//2)
     txw, txh = draw.textsize(text, font=font)
     draw.rectangle([txpos, (left+txw, top)], outline=_TEXT_COLOR, fill=_TEXT_COLOR, width=_FONT_THICKNESS)
-    draw.text(txpos, text, font=font, fill=_TEXT_COLOR)
+    draw.text(txpos, text, font=font, fill=(0,0,0))
     
     
     image = np.array(pil_image)

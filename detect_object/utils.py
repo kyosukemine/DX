651fe02f0bac64412feb3494b00cfb5b8c706743
kyosukemine <73222173+kyosukemine@users.noreply.github.com>
2021-11-04 19:50:26 +0900

    Update utils.py

--- detect_object/utils.py
+++ detect_object/utils.py
@@ -42,8 +42,8 @@ def visualize(
   """
   for detection in detections:
     # Draw bounding_box
-    start_point = detection.bounding_box.left, detection.bounding_box.top
-    end_point = detection.bounding_box.right, detection.bounding_box.bottom
+    start_point = 350-detection.bounding_box.left, 350-detection.bounding_box.top
+    end_point = 350-detection.bounding_box.right, 350-detection.bounding_box.bottom
     # cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
     
    

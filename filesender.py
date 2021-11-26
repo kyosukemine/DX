1054a8e9cfb65dbb34eb4c25e6ec608a463f5b5a
kyosukemine <k3.mine.0131@gmail.com>
2021-11-26 15:37:14 +0900

    add pc file

--- filesender.py
+++ filesender.py
@@ -4,12 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<html><h3>実習資料</h3><a href='/export'>ダウンロード</a></html>"
+    return "<html><h3>実習資料</h3><a href='/export/raspberrypi'>ダウンロードraspberry用</a><br><br><br><br><a href='/export/raspberrypi'>ダウンロードPC用</a></html>"
 
 
-@app.route("/export")
-def export_action():
-    filename = "送りたいファイルのパス"
+@app.route("/export/raspberrypi")
+def export_action_raspi():
+    filename = "送りたいファイルのパス/ラズパイ用"
+    return send_file(
+        path=filename,
+        as_attachment=True
+    )
+
+@app.route("/export/pc")
+def export_action_pc():
+    filename = "送りたいファイルのパス/パソコン用"
     return send_file(
         path=filename,
         as_attachment=True
